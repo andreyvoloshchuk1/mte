951b192669facc129bb39c06aab4559abeaa2112
Taras <tarasserhiytschuk@gmail.com>
2018-07-11 16:02:41 +0300

    fix errors

--- bin/mte-build.js
+++ bin/mte-build.js
@@ -17,23 +17,23 @@ const methods = config.methods;
 const isBuild = methods.isCreated(paths.currentPath, paths.build);
 
 const thumbsSettings = {
-    name: '200x150',
-    format: 'jpg',
-    screenSize: {
-        width: 1280,
-        height: 720
-    },
-    thumbSize: {
-        width: 200,
-        height: 150
-    }
+  name: '200x150',
+  format: 'jpg',
+  screenSize: {
+    width: 1280,
+    height: 720,
+  },
+  thumbSize: {
+    width: 200,
+    height: 150,
+  },
 };
 
 const autoprefixConfig = {
-    browsers: [
-        "ie >= 11",
-        "iOS 8.1" // DON'T DELETE, IT'S FOR PHANTOMJS
-    ]
+  browsers: [
+    "ie >= 11",
+    "iOS 8.1", // DON'T DELETE, IT'S FOR PHANTOMJS
+  ],
 };
 
 
@@ -42,27 +42,27 @@ const autoprefixConfig = {
  */
 
 commander
-    .usage('<filter> [UKR, RUS]')
-    .option('-L', '--lang', 'use --lang for generate single-lang project');
+  .usage('<filter> [UKR, RUS]')
+  .option('-L', '--lang', 'use --lang for generate single-lang project');
 
 /**
  * Help.
  */
 
 commander.on('--help', () => {
-    console.log('\n  Examples:\n');
-    console.log(chalk.gray('    # bulid all slides to production'));
-    console.log('    $ mte build\n');
-    console.log(chalk.gray('    # bulid filtered slides to production'));
-    console.log('    $ mte build --filter 2_flow');
-    console.log('    $ mte build -f 1_EXAM_C2_18_UKR_sl_05');
-    console.log('    $ mte build UKR/1_flow');
-    console.log('    $ mte build RUS');
+  console.log('\n  Examples:\n');
+  console.log(chalk.gray('    # bulid all slides to production'));
+  console.log('    $ mte build\n');
+  console.log(chalk.gray('    # bulid filtered slides to production'));
+  console.log('    $ mte build --filter 2_flow');
+  console.log('    $ mte build -f 1_EXAM_C2_18_UKR_sl_05');
+  console.log('    $ mte build UKR/1_flow');
+  console.log('    $ mte build RUS');
 });
 
 function help() {
-    commander.parse(process.argv);
-    if (commander.args.length > 1) return commander.help()
+  commander.parse(process.argv);
+  if (commander.args.length > 1) return commander.help()
 }
 
 help();
@@ -72,20 +72,20 @@ help();
  */
 
 if (isBuild) {
-    inquirer.prompt({
-        type: 'confirm',
-        name: 'ok',
-        message: 'Project has already builded. Do you want build again?\n' +
-        '  Files in ' + chalk.cyan(paths.currBuild) + ' will be ' + chalk.red('rewrite')
-    }).then(answer => {
-        if (answer.ok) {
-            // Clear Build Directory
-            // clearBuildDir();
-            run();
-        }
-    })
+  inquirer.prompt({
+    type: 'confirm',
+    name: 'ok',
+    message: 'Project has already builded. Do you want build again?\n' +
+    '  Files in ' + chalk.cyan(paths.currBuild) + ' will be ' + chalk.red('rewrite'),
+  }).then(answer => {
+    if (answer.ok) {
+      // Clear Build Directory
+      // clearBuildDir();
+      run();
+    }
+  })
 } else {
-    run();
+  run();
 }
 
 /**
@@ -93,32 +93,10 @@ if (isBuild) {
  */
 
 function run() {
-    let filter = commander.args[0];
-    const slides = eachSlide.getSlidesPaths(filter);
+  let filter = commander.args[ 0 ];
+  const slides = eachSlide.getSlidesPaths(filter);
 
-    if (filter === undefined) build(slides);
-
-    if (slides.length === 0) {
-        console.log(chalk.red('No matches!'));
-        process.exit(0);
-    } else {
-        console.log('------------------------------------------------------------------------------------')
-        slides.forEach(slide => console.log(slide));
-        console.log('------------------------------------------------------------------------------------')
-
-        inquirer.prompt({
-            type: 'confirm',
-            name: 'ok',
-            message: 'Build this slides?'
-        }).then(answer => {
-            if (answer.ok) {
-                build(slides)
-            }
-            else {
-                process.exit(0)
-            }
-        })
-    }
+  build(slides);
 }
 
 /**
@@ -127,25 +105,25 @@ function run() {
 
 function build(slides) {
 
-    autoprefixer(slides, autoprefixConfig);
+  autoprefixer(slides, autoprefixConfig);
 
-    new thumbMaker(slides, thumbsSettings)
-        .run()
-        .then(res => {
-            archiveMaker(slides)
-                .then(() => {
-                    console.log('\nBuild complete.');
-                    process.exit(0)
-                })
-                .catch(err => console.log(err));
+  new thumbMaker(slides, thumbsSettings)
+    .run()
+    .then(res => {
+      archiveMaker(slides)
+        .then(() => {
+          console.log('\nBuild complete.');
+          process.exit(0)
         })
         .catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
 }
 
 function clearBuildDir() {
-    const contents = methods.getContent(paths.currBuild);
+  const contents = methods.getContent(paths.currBuild);
 
-    contents.forEach(content => {
-        methods.rmDir(path.resolve(paths.currentPath, paths.currBuild), content)
-    })
+  contents.forEach(content => {
+    methods.rmDir(path.resolve(paths.currentPath, paths.currBuild), content)
+  })
 }
